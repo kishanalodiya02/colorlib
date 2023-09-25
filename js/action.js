0d60313eed9d5ed4fc2192caecbd0cef61cb404a
kishanalodiya02 <144224316+kishanalodiya02@users.noreply.github.com>
2023-09-25 00:04:44 -0700

    colorlib4

---
+++ js/action.js
@@ -0,0 +1,45 @@
+var show = document.querySelector(".second-ul");
+var menu =document.querySelector(".menu-bar");
+       
+          function db(){
+
+        
+        
+
+    //  if(menu.firstElementChild.classList.contains("fa-bars")){
+    //     menu.firstElementChild.classList.replace("fa-bars","fa-times")
+    //  }    else{
+    //     menu.firstElementChild.classList.replace("fa-times","fa-bars")
+    //  }
+
+
+
+
+    show.setAttribute("style","display:block;")
+
+    
+    // show.setAttribute("style","visibility:visible transition: 1s;")
+
+
+    // show.setAttribute("style","transform: translateX(0%); transition: 1s; ")
+     
+    //  show.style.display="block";
+    //  show.classList.toggle("dblock");
+        // show.classList.add("dblock");
+         
+        // show.style.visibility="visible"
+
+    
+   
+       
+          }
+
+          function db2(){
+            // show.classList.remove("dblock");
+            show.removeAttribute("style")
+
+          }
+          
+
+          
+    
